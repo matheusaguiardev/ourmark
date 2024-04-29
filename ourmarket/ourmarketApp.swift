8685870e54cd1541553c5fa6ab579b9a7b03ec1c
matheusaguiardev <joaomatheusaguiar@gmail.com>
2024-04-29 00:19:57 -0300

    Initial Commit

---
+++ ourmarket/ourmarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ourmarketApp.swift
+//  ourmarket
+//
+//  Created by Matheus Aguiar on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ourmarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
